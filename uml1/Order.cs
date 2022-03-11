f9fd1f37d4502da6d384d213d9fa2aa996c94d75
Bruger <Bruger@LAPTOP-N5A3KFDD>
2022-03-11 11:00:23 +0100

    Add project files.

---
+++ uml1/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace order
+{
+    class odrer()
+
+    double _taxpct;
+    double _deliverycosts;
+        
+    {
+    }
+}
---
+++ uml1/pizza.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pizza
+     class pizza
+{
+  
+    string _name;
+    int _price;
+
+    public pizza()
+    {
+        _name = "";
+        _price = 0;
+    }
+    public string name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public int price 
+    {
+        get { return _price; }
+        set { _price = value; }
+}
